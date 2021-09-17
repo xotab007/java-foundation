932560c6972bfba25bb099a0fed1f648e4701ccd
Mikhail <mikhail.kovalenko96@gmail.com>
2021-09-17 11:58:56 +0300

    Add Ball, update README.md

---
+++ src/javafoundation/section_2/Ball.java
@@ -0,0 +1,32 @@
+package javafoundation.section_2;
+
+public class Ball {
+    private double x;
+    private double y;
+
+    public Ball(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void move (double xDisp, double yDisp) {
+
+    }
+    
+}
