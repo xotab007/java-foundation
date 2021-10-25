b1bb6bac550872f1553c8210c7fc7eb8f52c6334
Mikhail <mikhail.kovalenko96@gmail.com>
2021-10-25 10:17:12 +0300

    Online_Sem_Demo

---
+++ src/javafoundation/online_sem/Demo_MinMax.java
@@ -0,0 +1,14 @@
+package javafoundation.online_sem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Demo_MinMax {
+    public static void main(String[] args) {
+
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    System.out.println(Collections.max(numbers));
+    System.out.println(Collections.min(numbers));
+}
+}
---
+++ src/javafoundation/online_sem/Demo_Shuffle.java
@@ -0,0 +1,20 @@
+package javafoundation.online_sem;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Demo_Shuffle {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> lottery = new ArrayList<>(100);
+        for (int i = 1; i <= 100; i++){
+            lottery.add(i);
+        }
+
+        Collections.shuffle(lottery);
+        System.out.println("Первые 10 чисел: ");
+        for (int i = 0; i < 10; i++){
+            System.out.println(lottery.get(i));
+        }
+    }
+}
