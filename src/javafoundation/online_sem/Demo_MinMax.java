804fea4df3d93841cc7684c0a518b68800a48b40
Mikhail <mikhail.kovalenko96@gmail.com>
2021-10-25 13:49:06 +0300

    Online_Sem_Demo

--- src/javafoundation/online_sem/Demo_MinMax.java
+++ src/javafoundation/online_sem/Demo_MinMax.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class Demo_MinMax {
-    public static void main(String[] args) {
-
-    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-    System.out.println(Collections.max(numbers));
-    System.out.println(Collections.min(numbers));
-}
+public  class Demo_MinMax {
+    public static void main(java.lang.String[] args) {
+        ArrayList<Integer> num = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        System.out.println(Collections.max(num));
+        System.out.println(Collections.min(num));
+    }
 }
